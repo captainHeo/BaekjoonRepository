6a2a10aacd81f3f95fd2ec8e03267343698eaae5
captainHeo <95284639+captainHeo@users.noreply.github.com>
2022-07-02 02:26:52 +0900

    Update 괄호.java

--- 백준/Silver/9012. 괄호/괄호.java
+++ 백준/Silver/9012. 괄호/괄호.java
@@ -21,7 +21,10 @@ public class Main {
             //입력 명령어 공백으로 쪼개 토큰 만들기
             String s = br.readLine();
 
+            //입력 문자열 크기만큼 반복
             for(int i=0; i<s.length(); i++) {
+
+
                 if(s.charAt(i) == '(')
                     push(s.charAt(i));
                 else{
@@ -30,20 +33,23 @@ public class Main {
 
             }
 
+            //중괄호 쌍이 서로 맞으면 스택이 비어있으니 YES 아니면 NO
             if(stack.isEmpty())
                 sb.append("YES").append('\n');
             else
                 sb.append("NO").append('\n');
 
+            //한 줄에 하나의 스택 계산이니 한 줄 끝나면 비워주기
             stack.clear();
         }
         System.out.println(sb);
     }
-
+    //(면 스택에 푸시
     public static void push(char c) {
         stack.push(c);
     }
 
+    //)면 스택이 비어있지않고 peek가 (면 삭제, 그렇지 않으면 )도 푸시
     public static void pop(char c) {
         if(!stack.isEmpty() && stack.peek() == '(')
             stack.pop();
