e4ce2c9f282527e124c00be568a3206b311db2d8
captainHeo <95284639+captainHeo@users.noreply.github.com>
2022-09-18 00:19:30 +0900

    [Bronze I] Title: N번째 큰 수, Time: 176 ms, Memory: 15616 KB -BaekjoonHub

--- 백준/Bronze/2693. N번째 큰 수/N번째 큰 수.java
+++ 백준/Bronze/2693. N번째 큰 수/N번째 큰 수.java
@@ -1,25 +1,35 @@
-import java.util.Arrays;
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuffer sb = new StringBuffer();
+        StringTokenizer st;
+        ArrayList<Integer> ar;
+        int N = Integer.parseInt(br.readLine());
 
-        int[] arr = new int[10];
+        for(int i=0; i<N; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
 
-        int[] results = new int[n];
+            ar = new ArrayList<>();
 
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < 10; j++) {
-                arr[j] = sc.nextInt();
+            while (st.hasMoreElements()) {
+
+                ar.add(Integer.parseInt(st.nextToken()));
             }
-            Arrays.sort(arr);
-            results[i] = arr[7];
-        }
 
-        for (int i = 0; i < n; i++) {
-            System.out.println(results[i]);
-        }
+            Collections.sort(ar, Collections.reverseOrder());
+
+            ar.remove(0); ar.remove(0);
+
+            sb.append(ar.get(0) + "\n");
+         }
+
+        System.out.println(sb);
     }
 }
