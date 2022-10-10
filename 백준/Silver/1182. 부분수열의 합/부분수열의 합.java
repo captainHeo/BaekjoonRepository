38238689c9175294fdc4e525f798a65427290397
captainHeo <95284639+captainHeo@users.noreply.github.com>
2022-10-10 16:25:05 +0900

    [Silver II] Title: 부분수열의 합, Time: 132 ms, Memory: 14192 KB -BaekjoonHub

--- 백준/Silver/1182. 부분수열의 합/부분수열의 합.java
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.java
@@ -1,31 +1,43 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
-class Main {
-    static int[] num;
-    private static int N; // 정수의 개수
-    private static int S; // 정수의 합
-    private static int answer = 0;
+public class Main {
+    static int ar[], N, S, count = 0;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        N = sc.nextInt();
-        S = sc.nextInt();
-        num = new int[N];
-        for (int i = 0; i < N; i++) {
-            num[i] = sc.nextInt();
+        N =  Integer.parseInt(st.nextToken());
+        S = Integer.parseInt(st.nextToken());
+        ar = new int[N];
+
+        st = new StringTokenizer(br.readLine(), " ");
+        for(int i=0; i<N; i++) {
+
+            ar[i] = Integer.parseInt(st.nextToken());
         }
-        dfs(0, 0);
-        if (S == 0) System.out.println(answer - 1); // S 0일때 처리 
-         else  System.out.println(answer);
 
+        DFS(0, 0);
+
+        // S 0일때, 공집합도 합이 0이므로 공집합 빼줘야함
+        if (S == 0)
+            System.out.println(count - 1);
+        else
+            System.out.println(count);
     }
 
-    private static void dfs(int depth, int sum) {
-        if (depth == N) {
-            if (sum == S) answer++;
+    private static void DFS(int l, int sum) {
+
+        //레벨과 N 같으면
+        if (l== N) {
+            if (sum == S)
+                count++;
             return;
         }
-        dfs(depth + 1, sum + num[depth]); // 1. 해당 인덱스 더함
-        dfs(depth + 1, sum); // 2. 해당 인덱스 더하지않음
+        //레벨은 똑같이 증가
+        DFS(l + 1, sum + ar[l]); // 1. 해당 인덱스 더함
+        DFS(l + 1, sum); // 2. 해당 인덱스 더하지않음
     }
 }
