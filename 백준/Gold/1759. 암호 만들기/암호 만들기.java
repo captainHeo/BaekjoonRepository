60a7a82ead63b6c2a230503ed330b15c71bc0e17
captainHeo <95284639+captainHeo@users.noreply.github.com>
2022-10-10 14:19:49 +0900

    [Gold V] Title: 암호 만들기, Time: 128 ms, Memory: 14240 KB -BaekjoonHub

--- 백준/Gold/1759. 암호 만들기/암호 만들기.java
+++ 백준/Gold/1759. 암호 만들기/암호 만들기.java
@@ -1,61 +1,76 @@
-import java.util.Arrays;
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
 public class Main {
 
-	static boolean check(String password) {
-		int vowel=0; //모음
-		int consonant=0;//자음 
-		for(char arr : password.toCharArray()) {
-			if(arr== 'a' || arr=='e' || arr=='i' || arr=='o' || arr=='u') {
-				vowel++;
-			}
-			else {
-				consonant++;
-			}
-		}
-		
-		return vowel>=1 && consonant>=2;
-	}
-	
-	
-	static void go(int n, String [] alpha, String password, int index) {
-		if(password.length() == n ) {
-			if(check(password)) {
-				System.out.println(password);
-			}
-			return;
-			
-			
-		}
-		
-		if( index >= alpha.length) {
-			return;
-		}
-		
-		
-		go(n,alpha,password+alpha[index],index+1);
-		go(n,alpha,password,index+1);
-		
-	}
-	
-	
-	public static void main(String[] args) {
+    public static int L, C;
+    public static char[] list;
+    public static char[] code;
 
-		Scanner sc= new Scanner(System.in);
-		int L=sc.nextInt();
-		int C=sc.nextInt();
+    public static void main(String[] args) throws IOException {
 
-		String [] alpha = new String[C];
-		
-		for(int i=0; i<C; i++) {
-			alpha[i] = sc.next();
-		}
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
 
-		Arrays.parallelSort(alpha);
-		
-		go(L,alpha,"",0);
-		
-	}
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        L = Integer.parseInt(st.nextToken());
+        C = Integer.parseInt(st.nextToken());
+
+        list = new char[C];
+        code = new char[L];
+
+        st = new StringTokenizer(br.readLine());
+
+        for (int x = 0; x < C; x++) {
+            list[x] = st.nextToken().charAt(0);
+        }
+
+
+        // 정렬
+        Arrays.sort(list);
+
+        makeCode(0,0);
+
+    }
+
+    public static void makeCode(int x,int idx) {
+
+
+        if (idx == L) {
+            // 최소 한개의 모음, 최소 2개의 자음인지 확인
+            if (isValid()) {
+                System.out.println(code);
+            }
+            return;
+        }
+
+        // 아직 길이 L의 코드를 만들지 않았고 글자도 아직 남았다.
+
+        for (int i = x; i < C; i++) {
+            code[idx] = list[i];
+            makeCode(i+1, idx + 1);
+        }
+    }
+
+    // 최소 모음 1개, 최소 자음 2개인지 확인
+    public static boolean isValid() {
+        int mo = 0;
+        int ja = 0;
+
+        for (char x : code) {
+            if (x == 'a' || x == 'e' || x == 'i' || x == 'o' || x == 'u') {
+                mo++;
+            } else {
+                ja++;
+            }
+        }
+
+        if (mo >= 1 && ja >= 2) {
+            return true;
+        }
+        return false;
+    }
 
 }
