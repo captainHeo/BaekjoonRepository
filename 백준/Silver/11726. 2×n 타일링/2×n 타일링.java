464878276e11c06b68239694a8644873afb6a33a
jinjinZara-98 <95284639+jinjinZara-98@users.noreply.github.com>
2022-11-06 00:57:39 +0900

    [Silver III] Title: 2×n 타일링, Time: 128 ms, Memory: 14300 KB -BaekjoonHub

--- 백준/Silver/11726. 2×n 타일링/2×n 타일링.java
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.java
@@ -2,7 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Main  {
+public class Main {
     static int dp[] = new int[1001];
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -15,7 +15,7 @@ public class Main  {
     private static int dp(int N) {
 
         if(N == 1)
-            return  1;
+            return 1;
         if(N == 2)
             return 2;
         if(dp[N] != 0)
@@ -24,4 +24,3 @@ public class Main  {
         return dp[N] = (dp(N -1) + dp(N -2)) % 10007;
     }
 }
-
