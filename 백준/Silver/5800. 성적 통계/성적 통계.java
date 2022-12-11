c1e2ae46d11ab536f73a9679bd187f8552f76cd5
Jin Heo <heojin0620@naver.com>
2022-12-11 14:20:13 +0900

    [Silver V] Title: 성적 통계, Time: 172 ms, Memory: 14920 KB -BaekjoonHub

--- 백준/Silver/5800. 성적 통계/성적 통계.java
+++ 백준/Silver/5800. 성적 통계/성적 통계.java
@@ -1,35 +1,40 @@
-import java.util.*;
- 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.StringTokenizer;
+
 public class Main {
-    public static void main(String args[]) {
-        Scanner sc = new Scanner(System.in);
-        StringBuilder sb = new StringBuilder();
-        int K = sc.nextInt();
-        for (int i = 0; i < K; i++) {
-            int N = sc.nextInt();
- 
-            Integer arr[] = new Integer[N];
- 
-            for (int j = 0; j < N; j++) {
-                arr[j] = sc.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuffer sb = new StringBuffer();
+        ArrayList<Integer> ar;
+        int K = Integer.parseInt(br.readLine());
+
+        StringTokenizer st; int N;
+        for(int i=0; i<K; i++) {
+
+            st = new StringTokenizer(br.readLine());
+
+            N = Integer.parseInt(st.nextToken());
+
+            ar = new ArrayList<>();
+            for(int j=0; j<N; j++) {
+                ar.add(Integer.parseInt(st.nextToken()));
             }
- 
-            Arrays.sort(arr, Collections.reverseOrder());
- 
-            int max = arr[0];
-            int min = arr[N - 1];
- 
-            int sub = Integer.MIN_VALUE;
- 
-            for (int j = 0; j < arr.length - 1; j++) {
-                sub = Math.max(arr[j] - arr[j + 1], sub);
+
+            Collections.sort(ar, Comparator.reverseOrder());
+
+            int differ = Integer.MIN_VALUE;
+            for(int k=0; k<ar.size()-1; k++) {
+                differ = Math.max(differ, ar.get(k) - ar.get(k+1));
             }
- 
-            sb.append("Class " + (i + 1) + "\n");
-            sb.append("Max " + max + ", " + "Min " + min + ", " + "Largest gap " + sub + "\n");
+            sb.append("Class " + (i+1) +  "\n").append("Max " + ar.get(0))
+                    .append(", Min " + ar.get(ar.size()-1)).append(", Largest gap " + differ + '\n');
         }
- 
+
         System.out.println(sb);
-        sc.close();
     }
 }
